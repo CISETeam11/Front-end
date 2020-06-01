f7195102896b7bf9be6544869cd742c449e49fa0
Zak Herman <zakanator@gmail.com>
2020-06-01 12:53:05 +1200

    Add bootstrap-vue support. Change table to b-table.

--- js/search.js
+++ js/search.js
@@ -1,6 +1,43 @@
 //:: Handles searchbar input :://
 const URL_ATRICLES = "https://app-articles-ae-aut.azurewebsites.net/api/articles";
 
+const vueTable = new Vue({
+    el:'#search_result',
+    data:{
+        isBusy: false,
+        sortBy: 'article.title',
+        sortDesc: false,
+        articles:[],
+        fields: [
+            {
+              key: 'article.title',
+              label: 'Title',
+              sortable: true
+            },
+            {
+              key: 'article.author',
+              label: 'Author',
+              sortable: true
+            },
+            {
+              key: 'article.year',
+              label: 'Year',
+              sortable: true
+            },
+            {
+              key: 'article.doi',
+              label: 'DOI',
+              sortable: true
+            },
+            {
+              key: 'results.result',
+              label: 'Result',
+              sortable: true
+            }
+        ]
+    }
+});
+
 //:: A simple function to show user messages ::/
 function showMessage(message, type) {
     var errorMessage = document.getElementById("error-msg");
@@ -28,13 +65,15 @@ function searchbarOnEnter() {
 
 //:: A function to get input from searchbar to get results from API :://
 function handleSearch() {
-    var articles = [];// An array to hold the filtered atricles.
     var searchbarInput = document.getElementsByClassName('search-input')[0].value;
     searchbarInput = searchbarInput.toUpperCase();
     if (!validateSearchbar(searchbarInput)) {
         return;
     }
+
+    vueTable.isBusy = true;
     var client = new HttpClient();// Calling api to get atricles.
+
     client.get(URL_ATRICLES, function (response) {
         const articles = JSON.parse(response);
         let results = [];
@@ -43,16 +82,11 @@ function handleSearch() {
             article.results.forEach(result => {
                 results.push({results: result, article: article});
             });
-
-            showMessage(articles.length + " article(s) found.");
         });
 
-        var vue = new Vue({
-            el:'#search_result',
-            data:{
-              articles: results
-            }
-        });
+        vueTable.articles = results;
+        vueTable.isBusy = false;
+        showMessage(articles.length + " article(s) found.");
     });
 }
 
