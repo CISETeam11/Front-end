8dbc55915ede6388facb5482600a790e2fe8899b
Grace0226 <53672597+Grace0226@users.noreply.github.com>
2020-06-11 21:20:05 +1200

    Update the moderation.js

--- js/moderation.js
+++ js/moderation.js
@@ -1,76 +1,63 @@
 // get an article in queue
+(async () => {
+  fetch('https://app-submissions-ae-aut.azurewebsites.net/api/moderation', { method: 'get' }).then(function (data) {
+    return data.json();
+  }).then(function (data) {
+    var queueArticles = [];
+    queueArticles.push(data);
+    queueForm.articles = queueArticles;
+  })
+})();
 
-fetch('https://app-submissions-ae-aut.azurewebsites.net/api/moderation', { method: 'get' }).then(function (data) {
-  return data.json();
-}).then(function (data) {
-  var articles = [data];
-  queueTable.articles = articles;
-  queueTable.isBusy = false;
-
-});
-
-const queueTable = new Vue({
+const queueForm = new Vue({
   el: '#queue',
   data: {
-    isBusy: true,
-    articles: [],
-    fields:
-      [{
-        key: "article.title",
-        label: "Title",
-      }, {
-        key: "article.author",
-        label: "Author",
-      },
-      {
-        key: "article.journal",
-        label: "Journal",
-      },
-      {
-        key: "article.year",
-        label: "Title",
-      },
-      {
-        key: "article.journalIssue",
-        label: "JournalIssue",
-      },
-      {
-        key: "article.volume",
-        label: "Volume",
-      },
-      {
-        key: "article.pages",
-        label: "Pages",
-      },
-      {
-        key: "article.doi",
-        label: "Doi",
-      },
-      {
-        key: "moderation",
-        label: "Moderation"
-
-      }]
+    articles: []
   },
-  methods:{
+  methods: {
     acceptArticle: function () {
-      console.log("yes")  
+      var title = this.articles[0].article.title;
+      var author = this.articles[0].article.author;
+      var journal = this.articles[0].article.journal;
+      var year = this.articles[0].article.year;
+      var doi = this.articles[0].article.doi;
+      var volume = this.articles[0].article.volume;
+      var pages = this.articles[0].article.pages;
+      var journalIssue = this.articles[0].article.journalIssue;
+      var id = this.articles[0].id;
+      var popReceipt = this.articles[0].popReceipt;
+
+      //console.log('{"article":{ "author": "' + author + '", "title": "' + title + '", "journal": "' + journal + '", "year": ' + year + ', "journalIssue": ' + journalIssue + ', "volume": ' + volume + ', "pages": "' + pages + '", "doi": "' + doi + '"}, "id": "' + id + '","popReceipt":"' + popReceipt + '"} ')
+      var data = '{"article":{ "author": "' + author + '", "title": "' + title + '", "journal": "' + journal + '", "year": ' + year + ', "journalIssue": ' + journalIssue + ', "volume": ' + volume + ', "pages": "' + pages + '", "doi": "' + doi + '"}, "id": "' + id + '","popReceipt":"' + popReceipt + '"} ';
+   
+      fetch('https://app-submissions-ae-aut.azurewebsites.net/api/analysis', {
+        method: 'POST', 
+        body: data, 
+        headers: new Headers({
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        })
+      }).then(res => res.json())
+      .catch(error => console.error('Error:', error))
+      .then(response => console.log('Success:', response));
+
     },
-    deleteArticle: function() {
-      console.log("no")
+    deleteArticle: function (id, popReceipt) {
+      console.log('{"id": "' + id + '","popReceipt":"' + popReceipt + '"}');
+      fetch('https://app-submissions-ae-aut.azurewebsites.net/api/moderation', {
+        method: 'DELETE',
+        body: '{"id": "' + id + '","popReceipt":"' + popReceipt + '"}',
+        headers: new Headers({
+          'Accept': '*',
+          'Content-Type': 'application/json'
+        })
+      }
+      ).then(function (data) {
+        alert("success");
+      });
     }
-      
-    
   }
 });
-
-
-
-
-
-
-
-
 
 // list all articles that need moderation
 (async () => {
@@ -106,7 +93,7 @@ const moderationTable = new Vue({
       },
       {
         key: "Year",
-        label: "Title",
+        label: "Year",
       },
       {
         key: "JournalIssue",
